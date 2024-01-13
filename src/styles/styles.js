f49809239ae04e624bdb986d57e958998f36edca
trinetraweb <syrohuwar@gmail.com>
2024-01-13 13:13:42 +0530

    JH

--- src/styles/styles.js
+++
@@ -1,5 +0,0 @@
-// const styles = {
-//     underlineAnimation : 'relative cursor-pointer overflow-hidden rounded rounded-bl-none duration-200 ease-linear transition-all before:absolute before:left-0 before:bottom-0 before:w-0 before:h-0 before:bg-blue-400 before:rounded-full before:ease-linear hover:before:w-full hover:before:h-full before:transition-all before:duration-200 hover:before:transition-all hover:before:duration-200 hover:before:scale-150 hover:before:ease-linear',
-// };
-
-// export default styles;
