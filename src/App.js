d3e5699b9206c3d527618b7dc0bc3d69af91c128
tnp-placements <156454286+tnp-placements@users.noreply.github.com>
2024-03-18 02:57:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Home from './components/Home/Home';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 
 function App() {
   return (
     <Router>
+    <SpeedInsights />
       <Routes>
         <Route exact path='/' element={<Home />} />
       </Routes>
