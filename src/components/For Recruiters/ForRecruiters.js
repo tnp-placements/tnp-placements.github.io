9d2f0139005424347125cd15c62f89d8b06c8e7f
tnp-placements <156454286+tnp-placements@users.noreply.github.com>
2024-03-18 02:16:09 +0530

    Update ForRecruiters.js

--- src/components/For Recruiters/ForRecruiters.js
+++ src/components/For Recruiters/ForRecruiters.js
@@ -19,35 +19,35 @@ const ForRecruiters = () => {
       <div className="px-10 2xl:px-40">
         <Heading2 text="For Recruiters" color="after:w-1/3" />
       </div>
-      // <Heading text="Placement Policies" styles="text-center hidden md:flex sm:mb-10" />
-      // <div className="text-center px-10">
-      //   The <strong>Placement Policy</strong> guidelines prevailing currently are as under:
-      // </div>
-      // <div className="flex-col hidden md:flex items-center border h-screen overflow-y-scroll overflow-scroll w-full md:w-fit mx-auto">
-      //   <AllPagesPdf pdf={PlacementPolicy} />
-      // </div>
-      // <div className="flex flex-col justify-center items-center gap-4 px-10 mb-6">
-      //   <a
-      //     href="https://drive.google.com/file/d/1Pt4W1LhT7jcR-lPprnJhOvuXxYR8f1Ka/view?usp=sharing"
-      //     target="_blank"
-      //     rel="noreferrer"
-      //     className="bg-[#0193DC] text-white px-4 py-2 md:hidden"
-      //   >
-      //     View
-      //   </a>
-      //   <button
-      //     className="bg-[#0193DC] text-white px-4 py-2"
-      //     onClick={() => {
-      //       downloadPdf();
-      //     }}
-      //   >
-      //     Download
-      //   </button>
-      // </div>
-      // <div className="flex flex-col xl:flex-row xl:items-center">
-      //   <div className="flex justify-center">
-      //       <img src={Procedure} alt="placement" className="max-w-[520px] w-full xl:w-auto" />
-      //   </div>
+      <Heading text="Placement Policies" styles="text-center hidden md:flex sm:mb-10" />
+      <div className="text-center px-10">
+        The <strong>Placement Policy</strong> guidelines prevailing currently are as under:
+      </div>
+      <div className="flex-col hidden md:flex items-center border h-screen overflow-y-scroll overflow-scroll w-full md:w-fit mx-auto">
+        <AllPagesPdf pdf={PlacementPolicy} />
+      </div>
+      <div className="flex flex-col justify-center items-center gap-4 px-10 mb-6">
+        <a
+          href="https://drive.google.com/file/d/1Pt4W1LhT7jcR-lPprnJhOvuXxYR8f1Ka/view?usp=sharing"
+          target="_blank"
+          rel="noreferrer"
+          className="bg-[#0193DC] text-white px-4 py-2 md:hidden"
+        >
+          View
+        </a>
+        <button
+          className="bg-[#0193DC] text-white px-4 py-2"
+          onClick={() => {
+            downloadPdf();
+          }}
+        >
+          Download
+        </button>
+      </div>
+      <div className="flex flex-col xl:flex-row xl:items-center">
+        <div className="flex justify-center">
+            <img src={Procedure} alt="placement" className="max-w-[520px] w-full xl:w-auto" />
+        </div>
         <div className="bg-[#F6F6F6] py-6 px-4 lg:py-16 lg:px-0 sm:px-16">
             <div className="px-2 sm:px-0 lg:px-14 mb-14">
                 <Heading2 text="Procedure" color="xl:after:w-0 after:w-0 text-[#0193DC] xl:text-center xl:w-full" />
