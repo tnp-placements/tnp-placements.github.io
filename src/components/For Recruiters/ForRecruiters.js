c455a99e9721930d62c8ec0eac22b801f904d4ac
tnp-placements <156454286+tnp-placements@users.noreply.github.com>
2024-03-18 02:32:05 +0530

    Update ForRecruiters.js

--- src/components/For Recruiters/ForRecruiters.js
+++ src/components/For Recruiters/ForRecruiters.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Heading2 from "../Heading2";
-import Heading from "../Heading";
-import AllPagesPdf from "../pdf/AllPagesPdf";
-// import PlacementPolicy from "../../abc.pdf";
-import { saveAs } from "file-saver";
+import ProcedureCard from "./ProcedureCard";
+// import AllPagesPdf from "../pdf/AllPagesPdf";
+// import { saveAs } from "file-saver";
 import Procedure from '../../images/Procedure.png';
 import { placementProcedure } from "../../styles/constant";
-import ProcedureCard from "./ProcedureCard";
 
 const ForRecruiters = () => {
 
@@ -19,14 +17,14 @@ const ForRecruiters = () => {
       <div className="px-10 2xl:px-40">
         <Heading2 text="For Recruiters" color="after:w-1/3" />
       </div>
-      <Heading text="Placement Policies" styles="text-center hidden md:flex sm:mb-10" />
+      {/* <Heading text="Placement Policies" styles="text-center hidden md:flex sm:mb-10" /> */}
       {/* <div className="text-center px-10">
         The <strong>Placement Policy</strong> guidelines prevailing currently are as under:
-      </div>
-      <div className="flex-col hidden md:flex items-center border h-screen overflow-y-scroll overflow-scroll w-full md:w-fit mx-auto">
+      </div> */}
+      {/* <div className="flex-col hidden md:flex items-center border h-screen overflow-y-scroll overflow-scroll w-full md:w-fit mx-auto">
         <AllPagesPdf pdf={PlacementPolicy} />
-      </div>
-      <div className="flex flex-col justify-center items-center gap-4 px-10 mb-6">
+      </div> */}
+      {/* <div className="flex flex-col justify-center items-center gap-4 px-10 mb-6">
         <a
           href="https://drive.google.com/file/d/1Pt4W1LhT7jcR-lPprnJhOvuXxYR8f1Ka/view?usp=sharing"
           target="_blank"
@@ -46,17 +44,17 @@ const ForRecruiters = () => {
       </div> */}
       <div className="flex flex-col xl:flex-row xl:items-center">
         <div className="flex justify-center">
-            <img src={Procedure} alt="placement" className="max-w-[520px] w-full xl:w-auto" />
+          <img src={Procedure} alt="placement" className="max-w-[520px] w-full xl:w-auto" />
         </div>
         <div className="bg-[#F6F6F6] py-6 px-4 lg:py-16 lg:px-0 sm:px-16">
-            <div className="px-2 sm:px-0 lg:px-14 mb-14">
-                <Heading2 text="Procedure" color="xl:after:w-0 after:w-0 text-[#0193DC] xl:text-center xl:w-full" />
-            </div>
-            <div className="flex flex-col lg:flex-row lg:flex-wrap gap-14 pl-2 lg:gap-14 xl:gap-8 lg:justify-between 2xl:justify-evenly lg:px-14 sm:px-10">
-                {placementProcedure.map((data)=> (
-                    <ProcedureCard data={data} key={data.number} />
-                ))}
-            </div>
+          <div className="px-2 sm:px-0 lg:px-14 mb-14">
+            <Heading2 text="Procedure" color="xl:after:w-0 after:w-0 text-[#0193DC] xl:text-center xl:w-full" />
+          </div>
+          <div className="flex flex-col lg:flex-row lg:flex-wrap gap-14 pl-2 lg:gap-14 xl:gap-8 lg:justify-between 2xl:justify-evenly lg:px-14 sm:px-10">
+            {placementProcedure.map((data) => (
+              <ProcedureCard data={data} key={data.number} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
