ea50e2b31597d2f21d693e2cca4c6f2f0ab51084
tnp-placements <156454286+tnp-placements@users.noreply.github.com>
2024-03-18 03:00:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Home from './components/Home/Home';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { SpeedInsights } from "@vercel/speed-insights/react";
+import { SpeedInsights } from "@vercel/speed-insights";
 
 
 function App() {
