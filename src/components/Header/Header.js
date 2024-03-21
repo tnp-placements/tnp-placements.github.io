8cc635047abc6bd1affa9870604757f96cca54ab
Utpalk904 <utpalk904@gmail.com>
2024-03-22 03:37:38 +0530

    Login changed and contact us button fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,23 +7,25 @@ const Header = () => {
 
   return (
     <header className={`headerBg h-[100svh] w-[100svw]`} id='home'>
-        <Navbar/>
-        <div className='text-white w-full h-[85%] flex flex-col justify-center xl:pl-32 lg:pl-28 md:pl-20 sm:pl-14 pl-8 gap-3'>
-          <div className='font-bold text-3xl sm:text-4xl md:text-4xl lg:text-5xl w-full leading-[1.15]'><h1>Netaji Subhas University<br/> Of Technology</h1></div>
-          <div className='lg:text-2xl md:text-xl sm:text-xl text-lg text-[#CECDCD] mb-2 sm:mb-4'><h2>Training And Placement Cell</h2></div>
-          <div><a
+      <Navbar />
+      <div className='text-white w-full h-[85%] flex flex-col justify-center xl:pl-32 lg:pl-28 md:pl-20 sm:pl-14 pl-8 gap-3'>
+        <div className='font-bold text-3xl sm:text-4xl md:text-4xl lg:text-5xl w-full leading-[1.15]'><h1>Netaji Subhas University<br /> Of Technology</h1></div>
+        <div className='lg:text-2xl md:text-xl sm:text-xl text-lg text-[#CECDCD] mb-2 sm:mb-4'><h2>Training And Placement Cell</h2></div>
+        <div className='flex flex-col md:flex-row gap-5'>
+          <a
             href="mailto:tnpcell@nsitonline.in"
-            className="px-10 py-2 duration-300 transition-all bg-[#0193DC] rounded-md font-semibold"
+            className="px-10 py-2 w-fit duration-300 transition-all bg-[#0193DC] rounded-md font-semibold"
           >
             Contact us(Students)
-          </a></div>
-              <div><a
+          </a>
+          <a
             href="mailto:placements@nsitonline.in"
-            className="px-10 py-2 duration-300 transition-all bg-[#0193DC] rounded-md font-semibold"
+            className="px-10 py-2 w-fit duration-300 transition-all bg-[#0193DC] rounded-md font-semibold"
           >
             Contact us(Recuiters)
-          </a></div>
+          </a>
         </div>
+      </div>
     </header>
   )
 }
--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import Logo from "../../images/NSUTlogo.png";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import './header.css';
+import { IoIosArrowDown } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 export default function Navbar({
   onHome,
@@ -10,10 +12,16 @@ export default function Navbar({
   onRecruiters,
 }) {
   const [activeSection, setActiveSection] = useState("home");
+  const [openLogin, setOpenLogin] = useState(false);
+  const [openLink, setOpenLink] = useState(false);
 
   const handleLinkClick = (section) => {
     setActiveSection(section);
   };
+
+  const handleClick = () => {
+    setOpenLogin(!openLogin);
+  }
 
   return (
     <div className="bg-transparent w-full">
@@ -22,22 +30,39 @@ export default function Navbar({
           <img src={Logo} alt="NSUT" className="w-[50px]" />
         </div>
         <div className="gap-6 text-sm lg:text-base hidden md:flex">
-          <a href="#home" className={`${activeSection === 'home'? 'active':''} link after:left-[5px]`} onClick={()=> handleLinkClick('home')}>Home</a>
-          <a href="#culture" className={`${activeSection === 'culture'? 'active':''} link after:left-[6px]`} onClick={()=> handleLinkClick('culture')}>Culture</a>
-          <a href="#academics" className={`${activeSection === 'academics'? 'active':''} link after:left-[9px]`} onClick={()=> handleLinkClick('academics')}>Academics</a>
-          <a href="#recruiters" className={`${activeSection === 'recruiters'? 'active':''} link after:left-[11px]`} onClick={()=> handleLinkClick('recruiters')}>For Recruiters</a>
-          <a href="https://admin.tnpnsut.in">For Coordinators</a>
+          <a href="#home" className={`${activeSection === 'home' ? 'active' : ''} link after:left-[5px]`} onClick={() => handleLinkClick('home')}>Home</a>
+          <a href="#culture" className={`${activeSection === 'culture' ? 'active' : ''} link after:left-[6px]`} onClick={() => handleLinkClick('culture')}>Culture</a>
+          <a href="#academics" className={`${activeSection === 'academics' ? 'active' : ''} link after:left-[9px]`} onClick={() => handleLinkClick('academics')}>Academics</a>
+          <a href="#recruiters" className={`${activeSection === 'recruiters' ? 'active' : ''} link after:left-[11px]`} onClick={() => handleLinkClick('recruiters')}>For Recruiters</a>
+          <div className={`${openLink && 'active'} relative flex gap-1 items-center duration-300 link after:left-[1px] cursor-pointer`} onClick={()=>setOpenLink(!openLink)}>
+            Downloads
+            <IoIosArrowDown className={`${openLink && 'rotate-180'} transition-all duration-150`} />
+            {/* <div className={`absolute ${openLink ? 'scale-1' : 'scale-0'} flex flex-col justify-center items-center border border-[#0193DC] top-[120%] right-0 left-0 md:left-[-8%] rounded-md w-full md:min-w-[180px] transition-all duration-200`}>
+              <Link to="https://student.tnpnsut.in" className="p-2 duration-300 w-full text-sm lg:text-base transition-all hover:bg-[#0193DC] font-medium lg:font-semibold border-b border-[#0193DC]" >
+                Student Login
+              </Link>
+              <Link to="https://admin.tnpnsut.in" className="p-2 duration-300 w-full text-sm lg:text-base transition-all hover:bg-[#0193DC] font-medium lg:font-semibold whitespace-nowrap" >
+                Coordinator Login
+              </Link>
+            </div> */}
+          </div>
         </div>
         <div>
-          <Link
-            to="https://student.tnpnsut.in"
-            className="px-10 py-2 duration-300 text-sm lg:text-base transition-all border border-[#0193DC] hover:bg-[#0193DC] rounded-md font-medium lg:font-semibold"
-          >
-            Student Section
-          </Link>
+          <div className="relative px-10 flex items-center gap-4 py-2 duration-200 text-sm lg:text-base border border-[#0193DC] rounded-md font-medium lg:font-semibold cursor-pointer" onClick={handleClick}>
+            Login
+            <IoIosArrowDown className={`${openLogin && 'rotate-180'} transition-all duration-150`} />
+            <div className={`absolute ${openLogin ? 'scale-1' : 'scale-0'} flex flex-col justify-center items-center border border-[#0193DC] top-[120%] right-0 left-0 md:left-[-8%] rounded-md w-full md:min-w-[180px] transition-all duration-200`}>
+              <Link to="https://student.tnpnsut.in" className="p-2 duration-200 w-full text-sm lg:text-base transition-all hover:bg-[#0193DC] font-medium lg:font-semibold border-b border-[#0193DC]" >
+                Student Login
+              </Link>
+              <Link to="https://admin.tnpnsut.in" className="p-2 duration-200 w-full text-sm lg:text-base transition-all hover:bg-[#0193DC] font-medium lg:font-semibold whitespace-nowrap" >
+                Coordinator Login
+              </Link>
+            </div>
+          </div>
         </div>
 
-        
+
         {/* <div className="md:hidden block">
           <img src={Logo} alt="NSUT" className="w-[50px]" />
         </div> */}
