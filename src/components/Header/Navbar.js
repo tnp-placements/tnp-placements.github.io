26381f661504721637ad4c9058b9fefb39cb348a
tnp-placements <156454286+tnp-placements@users.noreply.github.com>
2024-03-22 23:06:04 +0530

    Update Navbar.js

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -37,14 +37,14 @@ export default function Navbar({
           <div className={`${openLink && 'active'} relative flex gap-1 items-center duration-300 link after:left-[1px] cursor-pointer`} onClick={()=>setOpenLink(!openLink)}>
             Downloads
             <IoIosArrowDown className={`${openLink && 'rotate-180'} transition-all duration-150`} />
-            {/* <div className={`absolute ${openLink ? 'scale-1' : 'scale-0'} flex flex-col justify-center items-center border border-[#0193DC] top-[120%] right-0 left-0 md:left-[-8%] rounded-md w-full md:min-w-[180px] transition-all duration-200`}>
+            { <div className={`absolute ${openLink ? 'scale-1' : 'scale-0'} flex flex-col justify-center items-center border border-[#0193DC] top-[120%] right-0 left-0 md:left-[-8%] rounded-md w-full md:min-w-[180px] transition-all duration-200`}>
               <Link to="https://student.tnpnsut.in" className="p-2 duration-300 w-full text-sm lg:text-base transition-all hover:bg-[#0193DC] font-medium lg:font-semibold border-b border-[#0193DC]" >
                 Students
               </Link>
               <Link to="https://admin.tnpnsut.in" className="p-2 duration-300 w-full text-sm lg:text-base transition-all hover:bg-[#0193DC] font-medium lg:font-semibold whitespace-nowrap" >
                 Coordinators
               </Link>
-            </div> */}
+            </div> }
           </div>
         </div>
         <div>
