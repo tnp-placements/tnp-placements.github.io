1379f9847b30e0e14dbc5cebebe9cd48d8c1b0e8
tnp-placements <156454286+tnp-placements@users.noreply.github.com>
2024-07-15 23:55:18 +0530

    Update ForRecruiters.js

--- src/components/For Recruiters/ForRecruiters.js
+++ src/components/For Recruiters/ForRecruiters.js
@@ -19,33 +19,33 @@ const ForRecruiters = () => {
     <div className="flex w-full flex-col gap-10 py-5" id="recruiters">
       <div className="px-10 2xl:px-40">
         <Heading2 text="For Recruiters" color="after:w-1/3" />
-      </div>
-      <Heading text="Placement Policies" styles="text-center hidden md:flex sm:mb-10" />
-      <div className="text-center px-10">
-        The <strong>Placement Policy</strong> guidelines prevailing currently are as under:
-      </div>
-      {/* <div className="flex-col hidden md:flex items-center border h-screen overflow-y-scroll overflow-scroll"> */}
-        {/* <AllPagesPdf pdf={""} /> */}
-        <iframe title="T&P Guidelines" src="" className="md:w-full" style={{width:"60vw", height:"100vh", margin:"0 auto"}}/>
-      {/* </div> */}
-      <div className="flex flex-col justify-center items-center gap-4 px-10 mb-6">
-        <a
-          href=""
-          target="_blank"
-          rel="noreferrer"
-          className="bg-[#0193DC] text-white px-4 py-2 md:hidden"
-        >
-          View
-        </a>
-        <button
-          className="bg-[#0193DC] text-white px-4 py-2"
-          onClick={() => {
-            downloadPdf();
-          }}
-        >
-          Download
-        </button>
-      </div>
+      // </div>
+      // <Heading text="Placement Policies" styles="text-center hidden md:flex sm:mb-10" />
+      // <div className="text-center px-10">
+      //   The <strong>Placement Policy</strong> guidelines prevailing currently are as under:
+      // </div>
+      // {/* <div className="flex-col hidden md:flex items-center border h-screen overflow-y-scroll overflow-scroll"> */}
+      //   {/* <AllPagesPdf pdf={""} /> */}
+      //   <iframe title="T&P Guidelines" src="" className="md:w-full" style={{width:"60vw", height:"100vh", margin:"0 auto"}}/>
+      // {/* </div> */}
+      // <div className="flex flex-col justify-center items-center gap-4 px-10 mb-6">
+      //   <a
+      //     href=""
+      //     target="_blank"
+      //     rel="noreferrer"
+      //     className="bg-[#0193DC] text-white px-4 py-2 md:hidden"
+      //   >
+      //     View
+      //   </a>
+      //   <button
+      //     className="bg-[#0193DC] text-white px-4 py-2"
+      //     onClick={() => {
+      //       downloadPdf();
+      //     }}
+      //   >
+      //     Download
+      //   </button>
+      // </div>
       <div className="flex flex-col xl:flex-row xl:items-center">
         <div className="flex justify-center">
             <img src={Procedure} alt="placement" className="max-w-[520px] w-full xl:w-auto" />
