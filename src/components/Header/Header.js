06524885e8a1032b37abc8aee2f4afc10c105658
tnp-placements <156454286+tnp-placements@users.noreply.github.com>
2024-11-05 17:04:36 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,8 +9,8 @@ const Header=() => {
     <header className={`headerBg h-[100svh] w-[100svw]`} id='home'>
       <Navbar />
       <div className='text-white w-full h-[85%] flex flex-col justify-center xl:pl-32 lg:pl-28 md:pl-20 sm:pl-14 pl-8 gap-3'>
-        <div className='font-bold text-3xl sm:text-4xl md:text-4xl lg:text-5xl w-full leading-[1.15]'><h1>Netaji Subhas University<br /> Of Technology</h1></div>
-        <div className='lg:text-2xl md:text-xl sm:text-xl text-lg text-[#CECDCD] mb-2 sm:mb-4'><h2>Training And Placement Cell</h2></div>
+        <div className='font-bold text-3xl sm:text-4xl md:text-4xl lg:text-5xl w-full leading-[1.15]'><h1>Training And Placements</h1></div>
+        <div className='lg:text-2xl md:text-xl sm:text-xl text-lg text-[#CECDCD] mb-2 sm:mb-4'><h2>Netaji Subhas University<br /> Of Technology</h2></div>
         <div className='flex flex-col sm:flex-row gap-4 max-w-fit'>
           <a
             href="mailto:tnpcell@nsitonline.in"
