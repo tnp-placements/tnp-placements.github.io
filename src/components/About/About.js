7cbd62f117b1551d4206a1dc852edf294de553bf
tnp-placements <156454286+tnp-placements@users.noreply.github.com>
2024-11-05 16:54:57 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,8 +15,8 @@ const About = () => {
             <img src={StatueDesign} alt="ellipse" className='absolute left-2 sm:left-0 xl:left-5 xl:bottom-[-2px] bottom-2 sm:bottom-0 z-[-1]' />
           </div>
           <div className='flex flex-col gap-12 xl:gap-8 bg-[#F9FBFD] xl:w-full'>
-            <div className='text-[#0C101A] px-5 sm:px-10 xl:px-0'>{about.aboutText}</div>
-           <div className='text-[#123794] px-7 sm:px-12 xl:px-0'>{about.aboutTexts}</div>
+            <div className='text-[#0C101A]  px-5 sm:px-10 xl:px-0'>{about.aboutText}</div>
+           <div className='text-[#123794] font-weight-bold px-7 sm:px-12 xl:px-0'>{about.aboutTexts}</div>
            <div className='flex flex-col md:flex-row xl:justify-evenly items-center gap-4 md:gap-8 xl:gap-4 xl:flex-nowrap xl:px-0 md:justify-center md:px-6 md:flex-wrap'>
               {about.cardDetails.map((card)=>(
                 <AboutCard about={card} key={card.title}/>
